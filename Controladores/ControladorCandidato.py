18a12ef86d2601f19ce0ebf1c7098f334c9efaeb
Diego Alzate <diegoalzate.0207@gmail.com>
2023-04-09 22:57:50 -0500

    subiendo relacion uno a muchos

--- Controladores/ControladorCandidato.py
+++ Controladores/ControladorCandidato.py
@@ -1,39 +1,48 @@
 from Modelos.Candidato import Candidato
+from Modelos.Partido import Partido
 from Repositorios.RepositorioCandidato import RepositorioCandidato
+from Repositorios.RepositorioPartido import RepositorioPartido
 
 
 class ControladorCandidato():
-   """
+    """
    constructor que permite llevar a cabo la creacion de instancias del controlador.
    """
-   def __init__(self) -> object:
-       self.repositorioCandidato = RepositorioCandidato()
-       print("Creando ControladorCandidato")
 
+    def __init__(self):
+        self.repositorioCandidato = RepositorioCandidato()
+        self.repositorioPartido = RepositorioPartido()
+        print("Creando ControladorCandidato")
 
-   def index(self):
-       print("Listar todos los Candidatos")
-       return self.repositorioCandidato.findAll()
+    def index(self):
+        print("Listar todos los Candidatos")
+        return self.repositorioCandidato.findAll()
 
-   def create(self, elCandidato):
-       print("Crear un Candidato")
-       nuevoCandidato = Candidato(elCandidato)
-       return self.repositorioEstudiante.save(nuevoCandidato)
+    def create(self, elCandidato):
+        print("Crear un Candidato")
+        nuevoCandidato = Candidato(elCandidato)
+        return self.repositorioEstudiante.save(nuevoCandidato)
 
-   def show(self, id):
-       print("Mostrando un Candidato con id ", id)
-       elCandidato = Candidato(self.repositorioCandidato.findById(id))
-       return elCandidato.__dict__
+    def show(self, id):
+        print("Mostrando un Candidato con id ", id)
+        elCandidato = Candidato(self.repositorioCandidato.findById(id))
+        return elCandidato.__dict__
 
-   def update(self, id, elCandidato):
-       print("Actualizando Candidato con id ", id)
-       CandidatoActual = Candidato(self.repositorioCandidato.findById(id))
-       CandidatoActual.cedula = elCandidato["cedula"]
-       CandidatoActual.no_resolucion = elCandidato["no_resolucion"]
-       CandidatoActual.nombre = elCandidato["nombre"]
-       CandidatoActual.apellido = elCandidato["apellido"]
-       return self.repositorioCandidato.save(CandidatoActual)
+    def update(self, id, elCandidato):
+        print("Actualizando Candidato con id ", id)
+        CandidatoActual = Candidato(self.repositorioCandidato.findById(id))
+        CandidatoActual.cedula = elCandidato["cedula"]
+        CandidatoActual.no_resolucion = elCandidato["no_resolucion"]
+        CandidatoActual.nombre = elCandidato["nombre"]
+        CandidatoActual.apellido = elCandidato["apellido"]
+        return self.repositorioCandidato.save(CandidatoActual)
 
-   def delete(self, id):
-       print("Elimiando Candidato con id ", id)
-       return self.repositorioCandidato.delete(id)
+    def delete(self, id):
+        print("Elimiando Candidato con id ", id)
+        return self.repositorioCandidato.delete(id)
+
+    def asignarPartidos(self, id, id_partido):
+        candidatoActual = Candidato(self.repositorioCandidato.findById(id))
+        partidoActual = Partido(self.repositorioPartido.findById(id_partido))
+        candidatoActual.partido = partidoActual
+        return self.repositorioCandidato.save(candidatoActual)
